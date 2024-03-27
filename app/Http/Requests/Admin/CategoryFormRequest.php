01e8a88edcc1fc0aff90798824ec10d4bccf7ff0
malith94batugedara <malithbatugedara@gmail.com>
2024-03-27 12:55:13 +0530

    Category Insertion Added

---
+++ app/Http/Requests/Admin/CategoryFormRequest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+           
+            'name'=> [
+                'required',
+                'string',
+                'max:200'
+            ],
+            'slug'=> [
+                'required',
+                'string',
+                'max:200'
+            ],
+            'description'=> [
+                'required'
+            ],
+            'image'=> [
+                'required'
+            ],
+            'meta_title'=> [
+                'required'
+            ],
+            'meta_description'=> [
+                'required'
+            ],
+            'meta_keyword'=> [
+                'required'
+            ],
+            'navbar_status'=> [
+                'nullable'
+            ],
+            'status'=> [
+                'nullable'
+            ],
+
+        ];
+        return $rules;
+    }
+}
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,11 +4,51 @@
 
 @section('content')
 
+
 <div class="container-fluid px-4">
-    <h1 class="mt-4">Category</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item active">Category</li>
-    </ol>
+    <div class="card mt-4">
+          <div class="card-header">
+            <div class="row">
+               <div class="col-md-4"><h4>View Category</h4></div><div class="col-md-8"><a href="{{ route('admin.add-category') }}" class="btn btn-success float-end">Add Category</a></div>
+            </div>
+          </div>
+          <div class="card-body">
+                <div class="mt-3">
+                       @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                            </div>
+                       @endif
+                </div>
+                <table class="table table-bordered">
+                       <thead>
+                           <tr>
+                               <th>ID</th>
+                               <th>Name</th>
+                               <th>Image</th>
+                               <th>Status</th>
+                               <th>Action</th>
+                           </tr>
+                       </thead>
+                       <tbody>
+                        @foreach ($categories as $category)
+                            <tr>
+                               <td>{{$category->id}}</td>
+                               <td>{{$category->name}}</td>
+                               <td>
+                                   <img src="{{ asset('uploads/category/'.$category->image) }}" height="50px" width="50px" alt="Category Image"/>
+                               </td>
+                               <td>{{$category->status == 1 ? 'hidden' : 'shown' }}</td>
+                               <td>
+                                  <a href="" class="btn btn-success">Edit</a>
+                                  <a href="" class="btn btn-danger">Delete</a>
+                               </td>
+                            </tr>
+                        @endforeach
+                       </tbody>
+                </table>
+          </div>
+    </div>
 </div>
 
 @endsection
